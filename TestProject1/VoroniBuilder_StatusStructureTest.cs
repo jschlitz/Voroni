312b0c6ff7cce8bf4bad387452bd2c61200c7b43
Jacob Schlitz <jschlitz@gmail.com>
2012-04-21 21:36:45 -0400

    Added test base for status struct

---
+++ TestProject1/VoroniBuilder_StatusStructureTest.cs
@@ -0,0 +1,78 @@
+﻿using Geometry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject1
+{
+    
+    
+    /// <summary>
+    ///This is a test class for VoroniBuilder_StatusStructureTest and is intended
+    ///to contain all VoroniBuilder_StatusStructureTest Unit Tests
+    ///</summary>
+  [TestClass()]
+  public class VoroniBuilder_StatusStructureTest
+  {
+
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    // 
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //public static void MyClassInitialize(TestContext testContext)
+    //{
+    //}
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+    #endregion
+
+
+    /// <summary>
+    ///A test for StatusStructure Constructor
+    ///</summary>
+    [TestMethod()]
+    [DeploymentItem("Geometry.dll")]
+    public void VoroniBuilder_StatusStructureConstructorTest()
+    {
+      VoroniBuilder_Accessor.StatusStructure target = new VoroniBuilder_Accessor.StatusStructure();
+      Assert.Inconclusive("TODO: Implement code to verify target");
+    }
+  }
+}
